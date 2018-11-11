a0d329b2637a62a70576dea1b98e955923f19601
toshiya-i <toshiya.i@gmail.com>
2018-11-12 00:41:38 +0900

    add eol_viewer.py
    結果を確認するため表示機能を実装

---
+++ tests/test_eol_viewer.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from test.test_support import captured_stdout
+from tps.eol.eol_viewer import EndOfLifeViewer
+
+
+class TestEndOfLifeViewer(unittest.TestCase):
+
+    def test_view_for_cli_normal_case1(self):
+
+        normal_case1 = [
+            ["aa01", "bb01", "cc01", "2019-01-01", "bb01", "cc01"],
+            ["aa02", "bb02", "none", "2019-01-02", "bb02", "cc02"],
+            ["aa03", "bb03", u"cc三", "2019-01-03", "bb03", u"cc三"]
+        ]
+        with captured_stdout() as stdout:
+            EndOfLifeViewer().view_for_cli(normal_case1)
+            lines = stdout.getvalue().splitlines()
+
+        self.assertEqual(lines[0], 'aa01\tbb01\tcc01\t2019-01-01\tbb01\tcc01')
+        self.assertEqual(lines[1], 'aa02\tbb02\tnone\t2019-01-02\tbb02\tcc02')
+        self.assertEqual(lines[2], u'aa03\tbb03\tcc三\t2019-01-03\tbb03\tcc三')
+
+    def test_view_for_cli_error_case1(self):
+
+        error_case1 = [[1, 2, 3], [4, 5, 6]]
+        with self.assertRaises(TypeError):
+            EndOfLifeViewer().view_for_cli(error_case1)
+
+    def test_view_for_cli_error_case2(self):
+
+        error_case2 = [1, 2, 3]
+        with self.assertRaises(TypeError):
+            EndOfLifeViewer().view_for_cli(error_case2)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tps/eol/eol_viewer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class EndOfLifeViewer:
+
+    def view_for_cli(self, eol_list):
+
+        for eol_info in eol_list:
+            print('\t'.join(eol_info))
+
+
+if __name__ == '__main__':
+    normal_case1 = [
+        ["aa01", "bb01", "cc01", "2019-01-01", "bb01", "cc01"],
+        ["aa02", "bb02", "none", "2019-01-02", "bb02", "cc02"],
+        ["aa03", "bb03", u"cc三", "2019-01-03", "bb03", u"cc三"]
+    ]
+    EndOfLifeViewer().view_for_cli(normal_case1)
